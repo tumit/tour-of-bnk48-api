ff1d41e839309b7ab8f483867c9e00c75dd7027d
tumit <tumit@odd.works>
2018-01-11 10:37:18 +0700

    init project

---
+++ src/main/java/net/tumit/tourofbnk48api/TourOfBnk48ApiApplication.java
@@ -0,0 +1,12 @@
+package net.tumit.tourofbnk48api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOfBnk48ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourOfBnk48ApiApplication.class, args);
+    }
+}
