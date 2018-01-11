723cbca42806e4630a3611aa8b0dfaeed3856278
tumit <tumit@odd.works>
2018-01-11 11:29:14 +0700

    add member controller and model

--- src/main/java/net/tumit/tourofbnk48api/controller/MemberController.java
+++ src/main/java/net/tumit/tourofbnk48api/controller/MemberController.java
@@ -1,4 +1,11 @@
 package net.tumit.tourofbnk48api.controller;
 
+import net.tumit.tourofbnk48api.model.Member;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
+    public Member get(String slug) {
+        return new Member(1L, slug);
+    }
 }
--- src/main/java/net/tumit/tourofbnk48api/model/Member.java
+++ src/main/java/net/tumit/tourofbnk48api/model/Member.java
@@ -1,4 +1,9 @@
 package net.tumit.tourofbnk48api.model;
 
+import lombok.Data;
+
+@Data
 public class Member {
+    private final Long id;
+    private final String slug;
 }
